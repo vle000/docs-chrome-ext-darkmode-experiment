0a53b4be68cabb0b5c7af2896f1f65aaae319976
vle000 <vle12345@terpmail.umd.edu>
2026-03-12 20:51:32 -0400

    background.js change

--- ChromeToggleDarkMode/background.js
+++ ChromeToggleDarkMode/background.js
@@ -14,17 +14,22 @@ function applyDarkMode(tabId, enabled) {
   }
 }
 
+function isGoogleDocs(url) {
+  return url && url.startsWith("https://docs.google.com/document/u/");
+}
+
 chrome.runtime.onMessage.addListener((message, sender) => {
   if (message.action !== "toggle-dark") return;
 
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-    if (!tab?.id) return;
+    if (!tab?.id || !isGoogleDocs(tab.url)) return;
     applyDarkMode(tab.id, message.enabled);
   });
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status !== "complete") return;
+  if (!isGoogleDocs(tab.url)) return;
 
   chrome.storage.sync.get("darkEnabled", ({ darkEnabled }) => {
     if (darkEnabled) {
